4254a7025d8f6377862d224ab351805915b2c10d
SebasD0696 <129336554+SebasD0696@users.noreply.github.com>
2025-05-06 10:47:52 -0500

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -71,8 +71,6 @@ app.get('/dashboard', (req, res) => {
     res.send(`Bienvenido ${req.user.name}`);
 });
 
-app.listen(3000, () => console.log('Servidor en http://localhost:3000'));
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
 
