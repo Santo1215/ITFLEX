0239c416c202233b3af341ca65c018da35bdbd7e
SebasD0696 <129336554+SebasD0696@users.noreply.github.com>
2025-05-06 10:31:20 -0500

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -73,6 +73,9 @@ app.get('/dashboard', (req, res) => {
 
 app.listen(3000, () => console.log('Servidor en http://localhost:3000'));
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
+
 // Render
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'build')));
