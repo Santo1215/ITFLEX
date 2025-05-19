e28c967d4d8b1708467fd170f94c2b265f1a1bff
Santo1215 <jesus2221922@correo.uis.edu.co>
2025-05-18 20:55:02 -0500

    RENDER YA FUNCIONAL

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000' || process.env.FRONTEND_PRUEBAS,
   credentials: true,
 }));
 
