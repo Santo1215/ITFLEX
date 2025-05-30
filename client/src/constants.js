60f1c8013783b3dacdd4a54f5d461cdd5221e26c
Santo1215 <jesus2221922@correo.uis.edu.co>
2025-05-30 00:58:54 -0500

    Cambios para que funcione en ITFLEX

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,13 @@
-export const API_URL = window.location.hostname === 'localhost'
-  ? 'http://localhost:5000'
-  : 'https://itflex.onrender.com';
+const hostname = window.location.hostname;
+
+export const API_URL =
+  hostname === 'localhost'
+    ? 'http://localhost:5000'
+    : hostname === 'itflex.onrender.com'
+    ? 'https://itflex.onrender.com'
+    : hostname === 'pruebasitflex.onrender.com'
+    ? 'https://pruebasitflex.onrender.com'
+    : 'https://itflex.onrender.com'; // valor por defecto
 
   export const getUserId = () => {
   return localStorage.getItem('userId');
